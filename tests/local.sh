6fc7367a472b0f484580872c35b26903f868f10b
Raajay Viswanathan <raajay.v@gmail.com>
2016-12-18 20:06:29 -0600

    Update the shell script to start ps workers, servers and scheduler locally

--- tests/local.sh
+++ tests/local.sh
@@ -17,21 +17,22 @@ arg="$@"
 export DMLC_PS_ROOT_URI='127.0.0.1'
 export DMLC_PS_ROOT_PORT=8000
 export DMLC_ROLE='scheduler'
-${bin} ${arg} &
+export PS_VERBOSE=4
+nohup ${bin} ${arg} > scheduler.log 2>&1&
 
 
 # start servers
 export DMLC_ROLE='server'
 for ((i=0; i<${DMLC_NUM_SERVER}; ++i)); do
     export HEAPPROFILE=./S${i}
-    ${bin} ${arg} &
+    nohup ${bin} ${arg} > server-${i}.log 2>&1&
 done
 
 # start workers
 export DMLC_ROLE='worker'
 for ((i=0; i<${DMLC_NUM_WORKER}; ++i)); do
     export HEAPPROFILE=./W${i}
-    ${bin} ${arg} &
+    nohup ${bin} ${arg} > worker-${i}.log 2>&1&
 done
 
 wait
